448c96136921561788f69138f97330297bc1497b
DeadTillNotice <92230433+DeadTillNotice@users.noreply.github.com>
2024-01-06 16:39:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function moveNoButton() {
+    // No additional functionality needed for this scenario
+}
+
+function showLoveMessage() {
+    document.body.style.backgroundColor = '#ffcccb';
+    document.querySelector('.question').innerText = 'Cool cool cool, I love you papao';
+    document.querySelector('.btn-group').style.display = 'none';
+}
