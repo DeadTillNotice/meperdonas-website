8c8de8f4916a11ca50ac76259ff7516fea34c420
DeadTillNotice <92230433+DeadTillNotice@users.noreply.github.com>
2024-01-06 17:07:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const wrapper = document.querySelector('.wrapper');
-const question = document.querySelector('.question');
-const yesBtn = document.querySelector('.yes-btn');
-const noBtn = document.querySelector('.no-btn');
-
-yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'Cool cool cool, I love you paopao';
-});
-
-noBtn.addEventListener('mouseover', () => {
-    const maxX = wrapper.offsetWidth - noBtn.offsetWidth;
-    const maxY = wrapper.offsetHeight - noBtn.offsetHeight;
+function moveNoButton() {
+    const maxX = window.innerWidth - 150;
+    const maxY = window.innerHeight - 40;
 
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
+    const noBtn = document.querySelector('.no-btn');
     noBtn.style.left = `${randomX}px`;
     noBtn.style.top = `${randomY}px`;
-});
+}
+
+function showLoveMessage() {
+    document.body.style.backgroundColor = '#ffcccb';
+    document.querySelector('.question').innerText = 'Cool cool cool, I love you paopao';
+    document.querySelector('.btn-group').style.display = 'none';
+}
