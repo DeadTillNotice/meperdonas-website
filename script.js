cb530359eb7f7129eee76475da25366b2df64877
DeadTillNotice <92230433+DeadTillNotice@users.noreply.github.com>
2024-01-06 16:37:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showLoveMessage() {
-    document.body.style.backgroundColor = '#ffcccb';
-    document.querySelector('h1').textContent = 'Cool cool cool, I love you papao';
-    document.querySelector('.buttons').style.display = 'none';
-}
-
