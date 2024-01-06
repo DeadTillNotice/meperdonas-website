e366c4d93746400919104ab863bfb530614585e3
DeadTillNotice <92230433+DeadTillNotice@users.noreply.github.com>
2024-01-06 16:23:57 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function moveNoButton() {
+    // No additional functionality needed for this scenario
+}
+
+function showLoveMessage() {
+    document.body.style.backgroundColor = '#ffcccb';
+    document.querySelector('h1').innerText = 'I love you more';
+    document.querySelector('.buttons').style.display = 'none';
+}
