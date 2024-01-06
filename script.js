660ade33f40ace022e42f49445976c53b90a5b42
DeadTillNotice <92230433+DeadTillNotice@users.noreply.github.com>
2024-01-06 16:33:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-function moveNoButton() {
-    // No additional functionality needed for this scenario
-}
-
 function showLoveMessage() {
     document.body.style.backgroundColor = '#ffcccb';
-    document.querySelector('h1').innerText = 'Cool cool cool, I love you papao';
+    document.querySelector('h1').textContent = 'Cool cool cool, I love you papao';
     document.querySelector('.buttons').style.display = 'none';
 }
+
